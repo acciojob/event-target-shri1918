e2925bef290cc61b7bd51086b1a2f865f8d036ae
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-05-07 14:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    this.listeners = new Map();
   }
-  
+
+  addEventListener(type, callback) {
+    if (!this.listeners.has(type)) {
+      this.listeners.set(type, []);
+    }
+
+    const typeListeners = this.listeners.get(type);
+
+    if (!typeListeners.some(listener => listener === callback)) {
+      typeListeners.push(callback);
+    }
+  }
+
+  removeEventListener(type, callback) {
+    if (this.listeners.has(type)) {
+      const typeListeners = this.listeners.get(type);
+      const index = typeListeners.findIndex(listener => listener === callback);
+      if (index !== -1) {
+        typeListeners.splice(index, 1);
+      }
+    }
+  }
+
+  dispatchEvent(event) {
+    if (this.listeners.has(event)) {
+      const typeListeners = this.listeners.get(event);
+      typeListeners.forEach(listener => listener());
+    }
+  }
+}
